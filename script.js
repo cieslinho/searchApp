fac72c2fc50e5f232f865d791988ee94d9dc4d4a
Szymon Cieśla <cszymon01@gmail.com>
2022-10-10 12:28:25 +0200

    created app

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const input = document.querySelector('.search')
-const allLi = document.querySelectorAll('li')
+const li = document.querySelectorAll('li')
 
-const inputCheck = () => {}
+const inputCheck = e => {
+	const text = e.target.value.toLowerCase()
+
+	console.log(text)
+	li.forEach(singleLi => {
+		const task = singleLi.textContent
+		if (task.toLowerCase().indexOf(text) !== -1) {
+			singleLi.style.display = 'block'
+		} else {
+			singleLi.style.display = 'none'
+		}
+	})
+}
 
 input.addEventListener('keyup', inputCheck)
+
+// alternatywa https://www.youtube.com/watch?v=B7PxyyF2uds&ab_channel=MMCSchool
